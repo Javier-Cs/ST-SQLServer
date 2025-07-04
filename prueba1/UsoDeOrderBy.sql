56600c089b1390f5d761467890c765d9d45be31c
Javier-Cs <krchipullajavier@gmail.com>
2025-07-04 13:12:11 -0500

    uso de order by en campos

---
+++ prueba1/UsoDeOrderBy.sql
@@ -0,0 +1,21 @@
+select * from Empleados;
+
+--- uso de order By
+--- de forma acendente y decen
+select * from Empleados order by EDAD;
+
+select * from Empleados order by EDAD desc;
+
+select * from Empleados order by idempleado;
+
+select * from Empleados order by idempleado desc;
+
+---  uso de order by y where
+select EDAD from Empleados
+where EDAD > 28
+order by EDAD;
+
+--- uso de distintos campos
+select nombre, APELLIDO, EDAD , SALARIO
+FROM Empleados
+order by EDAD desc, SALARIO asc;
