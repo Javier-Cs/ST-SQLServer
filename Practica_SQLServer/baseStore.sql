d59541e039c6a9fa871f0d87eec4f2b5891dc0fb
Javier-Cs <krchipullajavier@gmail.com>
2026-01-05 23:19:19 -0500

    modificacion para multiempresas

--- Practica_SQLServer/baseStore.sql
+++ Practica_SQLServer/baseStore.sql
@@ -1,12 +1,12 @@
 -- creacion de la base de datos
 if not exists(select * from sys.databases
-where name = 'TiendaR_bd')
+where name = 'TiendaOnline_bd')
 begin
-create database TiendaR_bd
+create database TiendaOnline_bd
 end;
 
 -- uso de la base de datos
-use TiendaR_bd
+use TiendaOnline_bd
 go
 
 -- creacion de esquema
@@ -23,22 +23,62 @@ drop table if exists store.Products;
 drop table if exists store.Categories;
 drop table if exists store.Brands;
 
+--- tabla empresa
+create table store.companies(
+    CompanyId int identity(1,1) not null,
+    Name nvarchar(150) not null,
+    LegalName nvarchar(200)not null,
+    TaxId nvarchar(50) not null,
+    Email nvarchar(200) not null,
+    Phone nvarchar(20),
+    Address nvarchar(300),
+    CreatedAt datetime default getdate(),
+    IsActive bit default(1),
+    constraint PK_CompanyId primary key (CompanyId),
+    constraint UQ_Company_TaxId unique (TaxId)
+);
+
+
+--- Tabla FACTURAS
+create table store.Invoices(
+    InvoiceId int identity(1,1) not null,
+    OrderId int not null,
+    CompanyId int not null,
+    InvoiceNumber nvarchar(50) not null,
+    InvoiceDate datetime default getdate(),
+    Subtotal decimal(10,2) not null,
+    Tax decimal(10,2) not null,
+    Total decimal(10,2) not null,
+    Status nvarchar(20) default 'emitida',
+    constraint PK_InvoiceId primary key (InvoiceId),
+    constraint FK_Invoice_Order foreign key (OrderId)
+        references store.Orders(OrderId),
+    constraint FK_Invoice_Company foreign key (CompanyId)
+        references store.Companies(CompanyId)
+);
+
 
 create table store.Categories(
 CategoryId int identity(1,1) not null,
 Name nvarchar(70) not null,
 Description nvarchar(max) not null,
 IsActive bit default (1),
-constraint UQ_CategoryName unique(Name),
-constraint PK_CategoryId primary key(CategoryId)
+CompanyId int not null,
+constraint UQ_CategoryName unique(CompanyId ,Name),
+constraint PK_CategoryId primary key(CategoryId),
+constraint  FK_Category_company foreign key (CompanyId)
+    references store.companies(CompanyId)
 );
 
 create table store.Brands(
 BrandId int identity(1,1) not null,
 Name nvarchar(100) not null,
 Description nvarchar(max) not null,
-constraint UQ_BrandName unique(Name),
-constraint PK_BrandId primary key(BrandId)
+CompanyId int not null,
+constraint UQ_BrandName unique(CompanyId ,Name),
+constraint PK_BrandId primary key(BrandId),
+constraint  FK_Brands_company foreign key (CompanyId)
+    references store.companies(CompanyId)
 );
 
 create table store.Products(
@@ -53,12 +93,15 @@ Dimensions nvarchar(200) not null,
 IsActive bit default (1),
 CategoryId int not null,
 BrandId int not null,
+CompanyId int not null,
 constraint PK_ProductId primary key(ProductId),
-constraint UQ_SKU unique(SKU),
+constraint UQ_SKU unique(CompanyId ,SKU),
 constraint FK_Category foreign key(CategoryId)
     references store.Categories(CategoryId),
 constraint FK_Brand foreign key(BrandId)
-    references store.Brands(BrandId)
+    references store.Brands(BrandId),
+constraint  FK_Product_company foreign key (CompanyId)
+    references store.companies(CompanyId)
 );
 
 create table store.Users(
@@ -74,12 +117,15 @@ constraint PK_UserId primary key(UserId)
 
 create table store.Inventory(
 InventoryId int identity(1,1) not null,
-ProductId int unique, -- Unique para relación 1 a 1 con Product
+ProductId int , -- Unique para relación 1 a 1 con Product
 Stock int,
 LastUpdated datetime default getdate(),
+CompanyId int not null,
 constraint PK_InventoryId primary key(InventoryId),
 constraint FK_Product_Inventory foreign key(ProductId)
-    references store.Products(ProductId)
+    references store.Products(ProductId),
+constraint  FK_Inventory_company foreign key (CompanyId)
+    references store.companies(CompanyId)
 );
 
 create table store.Orders(
@@ -87,12 +133,15 @@ OrderId int identity(1,1) not null,
 UserId int not null,
 OrderDate datetime default getdate(),
 TotalAmount decimal(10,2) not null,
-Status nvarchar(20) default 'enviado',
+Status nvarchar(20) default 'en proceso',
 PaymentMethod nvarchar(50) not null,
 ShippingCost decimal(10,2) not null,
+CompanyId int not null,
 constraint PK_OrderId primary key(OrderId),
 constraint FK_User_Order foreign key(UserId)
-    references store.Users(UserId)
+    references store.Users(UserId),
+constraint  FK_Orders_company foreign key (CompanyId)
+    references store.companies(CompanyId)
 );
 
 create table store.OrderItems(
@@ -107,3 +156,5 @@ constraint FK_Order_OrderItem foreign key(OrderId)
 constraint FK_Product_OrderItem foreign key(ProductId)
      references store.Products(ProductId)
 );
+
+
